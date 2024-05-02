5f2f214a0036f0019df80c4d9c57a36aabb97494
Shohaibur <shoaibrochi9@gmail.com>
2024-05-02 21:55:42 +0600

    same update 2

--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -5,12 +5,13 @@ import { Typography } from "@mui/material";
 const SliderSelect = () => {
   return (
     <>
-      <Typography variant="h6" fontFamily={"initial"} fontStyle={"oblique"} fontWeight={"normal"} >
+      <Typography variant="h7" fontFamily={"sans"} fontStyle={"oblique"} fontWeight={"normal"} >
         Home Value
       </Typography>
+      <Typography variant="h5" fontFamily={"serif"} fontWeight={"bold"}> $1000 </Typography >
       <SliderComponent min={10} max={60} defaultValue={3} step={5} onChange={(e, value) => console.log(value)} />
 
-      <Typography variant="h5" fontFamily={"serif"} fontWeight={"bold"}> $1000 </Typography >
+    
     </>
   )
 }
